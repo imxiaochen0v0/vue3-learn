0459a6124dbac671617d0ed668d6088d891f18d1
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-10 00:39:46 +0800

    子父通信

---
+++ src/components/子父通信/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import sonCom from '@/components/sonCom.vue'
+import { ref } from 'vue'
+
+const money = ref(1000)
+const addMoney = () => {
+  money.value += 100
+}
+const handleChangeMoney = (val) => {
+  money.value -= val
+}
+</script>
+
+<template>
+  <div class=''>
+    <h1>父组件 - {{ money }}
+      <button @click="addMoney">挣钱</button>
+    </h1>
+    <sonCom car="兰博基尼" :money="money" @changeMoney="handleChangeMoney"></sonCom>
+  </div>
+</template>
+
+<style scoped>
+button{
+  width: 100px;
+  height: 40px;
+  border: none;
+  border-radius: 10px;
+  background-color: #00CED1	;
+  color: white;
+  font-size: 18px;
+}
+</style>
---
+++ src/components/子父通信/sonCom.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  car: String,
+  money: Number
+})
+const emit = defineEmits([
+  'changeMoney'
+])
+console.log('🚀 ~ props:', props.car)
+console.log('🚀 ~ props:', props.money)
+
+const buy = () => {
+  emit('changeMoney', 100)
+}
+</script>
+
+<template>
+  <div class='sonCom'>
+    <h2>子组件
+      <button @click="buy">花钱</button>
+    </h2>
+    {{ car }} - {{ money }}
+  </div>
+</template>
+
+<style scoped>
+.sonCom{
+  border: 1px solid pink;
+  padding: 25px;
+}
+
+button{
+  width: 80px;
+  height: 30px;
+  border: none;
+  border-radius: 10px;
+  background-color: #00CED1	;
+  color: white;
+  font-size: 16px;
+}
+</style>
