e3dee88afe56777dbb3eca4cd67353f686cf4a9a
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-10 03:34:06 +0800

    模板引用

---
+++ .代码/模板引用/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import textCom from '@/components/textCom.vue';
+
+const inp = ref(null)
+const clickBtn = () => {
+  inp.value.focus()
+
+}
+
+const com = ref(null)
+const getCom = () => {
+  console.log(com.value.count)
+  com.value.sayHi()
+}
+</script>
+
+<template>
+  <div class=''>
+    <input type="text" ref="inp">
+    <button @click="clickBtn">点击</button>
+    <textCom ref="com"></textCom>
+    <button @click="getCom">获取组件</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ .代码/模板引用/textCom.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(520)
+const sayHi = () => {
+  console.log('你好')
+}
+
+defineExpose({
+  count, 
+  sayHi
+})
+</script>
+
+<template>
+  <div class=''>
+    测试组件 - {{ count }}
+  </div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
