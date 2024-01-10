524503cc534a7d7b642e120f200d3e379b88913d
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-10 20:56:54 +0800

    storeToRefs解决解构时响应式丢失

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,31 @@ import son1Com from '@/components/son1Com.vue'
 import son2Com from '@/components/son2Com.vue'
 import { useCountStore } from "@/store/counter"
 import { useChannelStore } from "@/store/channel"
+import { storeToRefs } from 'pinia';
 
 const countStore = useCountStore()
 const channelStore = useChannelStore()
+
+const { count, msg } = storeToRefs(countStore)
+const { channelsList } = storeToRefs(channelStore)
+// 解构方法不需要 storeToRefs
+const { getChannels } = channelStore
 </script>
 
 <template>
   <div class=''>
     <h1>根组件
-      - {{ countStore.count }}
-      - {{ countStore.msg }}
+      - {{ count }}
+      - {{ msg }}
     </h1>
     <son1Com></son1Com>
     <son2Com></son2Com>
+
     <hr>
-    <button @click="channelStore.getChannels">获取频道数据</button>
+
+    <button @click="getChannels">获取频道数据</button>
     <ul>
-      <li v-for='(item, index) in channelStore.channelsList' :key='index'>{{ item.name }}</li>
+      <li v-for='(item, index) in channelsList' :key='index'>{{ item.name }}</li>
     </ul>
   </div>
 </template>
--- src/components/son1Com.vue
+++ src/components/son1Com.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { useCountStore } from "@/store/counter"
+import { storeToRefs } from "pinia";
 
 const countStore = useCountStore()
+const { doubleCount } = storeToRefs(countStore)
 </script>
 
 <template>
   <div class=''>
-    子组件1- {{ countStore.count }} - {{ countStore.doubleCount }}
+    子组件1- {{ countStore.count }} - {{ doubleCount }}
     <button @click="countStore.addCount">+</button>
   </div>
 </template>
