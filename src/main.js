c7388c114b91e4c89fde5bdb0803c0cfe93799c2
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-10 18:10:30 +0800

    pinia安装

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
-
 // new Vue() 创建一个应用实例 => createApp()
 // 将创建实例封装 保证每个实例的独立封闭性
+import { createApp } from 'vue'
+import { createPinia } from 'pinia' 
 import App from './App.vue'
 
-// mount挂载点 #app(id为app的盒子)
-createApp(App).mount('#app')
+const pinia = createPinia() // 创建pinia实例
+const app = createApp(App) // 创建根实例
+
+app.use(pinia)
+app.mount('#app') // mount挂载点 #app(id为app的盒子)
