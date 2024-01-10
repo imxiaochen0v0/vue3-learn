6479ca398b7a23a09c8d5cb0e872d0a5deeb4c87
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-10 17:57:35 +0800

    defineModel

--- .代码/defineModel/App.vue
+++ .代码/defineModel/App.vue
@@ -1,13 +1,15 @@
 <script setup>
+import MyInput from '@/components/MyInput.vue';
+import { ref } from 'vue';
 
+const txt = ref('imxiaochen')
 </script>
 
 <template>
   <div class=''>
-    
+    <MyInput v-model="txt"></MyInput>
+    {{ txt }}
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
